ce648322794cdfddaeabba0cbbc3c6a28834c139
Victor <bulkin_viktor@mail.ru>
2023-04-12 14:53:06 +0300

    del unnesseccary, add some data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
-function App() {
+
+const options: Highcharts.Options = {
+  title: {
+    text: 'My chart',
+  },
+  series: [
+    {
+      name: 'vds_wsub',
+      type: 'line',
+      data: [
+        {
+          name: 'Астраханьгазпром',
+          x: 1.3181,
+          y: 20444.906195843847,
+        },
+        {
+          name: 'Когалымнефтегаз',
+          x: 1.4659870000000002,
+          y: 19547.57211652679,
+        },
+        {
+          name: 'Якутская ТЭК',
+          x: 1.4856474244894,
+          y: 17700.20810192281,
+        },
+        {
+          name: 'Сургутгаздобыча',
+          x: 4.999467424489399,
+          y: 16312.606703476185,
+        },
+      ],
+    },
+    {
+      name: 'vds_sub',
+      type: 'line',
+      data: [
+        {
+          name: 'Когалымнефтегаз',
+          x: 0.147887,
+          y: 29009.029327711363,
+        },
+        {
+          name: 'Астраханьгазпром',
+          x: 1.4659870000000002,
+          y: 25893.262836499664,
+        },
+        {
+          name: 'Якутская ТЭК',
+          x: 1.4856474244894,
+          y: 24810.482824211416,
+        },
+        {
+          name: 'Сургутгаздобыча',
+          x: 4.999467424489399,
+          y: 22179.304418474505,
+        },
+        {
+          name: 'Урайнефтегаз',
+          x: 5.055385424489399,
+          y: 19528.838387790784,
+        },
+        {
+          name: 'Волгограднефтепереработка',
+          x: 18.984285424489396,
+          y: 18951.12009989673,
+        },
+      ],
+    },
+  ],
+};
+
+
+const App = (props: HighchartsReact.Props) => {
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      ref={chartComponentRef}
+      {...props}
+    />
   );
-}
+};
 
 export default App;
